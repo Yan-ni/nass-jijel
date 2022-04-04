93f8d0ab19d311a8353c2eb976ebc98297d8e16a
Anis <anis01000001@gmail.com>
2022-04-04 15:29:09 +0100

    fear: display time remaining for iftar

--- assets/js/ramadan.js
+++ assets/js/ramadan.js
@@ -10,29 +10,109 @@ function getTodayAdhanTimes()
     .catch(error => console.error(error));
 } 
 
-function incrementStringof2(string)
+function intToStringof2(number)
 {
-  if(parseInt(string) >= 10)
-    return string;
+  if(parseInt(number) >= 10)
+    return number.toString();
 
-  return "0" + (parseInt(string) +1);
+  return "0" + number;
 }
+
+var iftarTime = '';
 
 async function displayTime()
 {
-  const iftarTime = document.querySelector('.iftar > .time');
-  const imsakTime = document.querySelector('.imsak > .time');
-  const hijriDate = document.querySelector('.hijriDate');
+  const iftarTimeEl = document.querySelector('.iftar > .time');
+  const imsakTimeEl = document.querySelector('.imsak > .time');
+  const hijriDateEl = document.querySelector('.hijriDate');
 
   const times = await getTodayAdhanTimes();
 
-  iftarTime.textContent = times.timings.Maghrib.split(' ')[0];
-  imsakTime.textContent = times.timings.Imsak.split(' ')[0];
+  iftarTime = times.timings.Maghrib.split(' ')[0];
+
+  iftarTimeEl.textContent = iftarTime;
+  imsakTimeEl.textContent = times.timings.Imsak.split(' ')[0];
 
   const hijri = times.date.hijri;
 
-  hijriDate.textContent = [hijri.weekday.ar, incrementStringof2(hijri.day), hijri.month.ar, hijri.year].join(' ');
+  hijriDateEl.textContent = [hijri.weekday.ar, intToStringof2(parseInt(hijri.day) +1), hijri.month.ar, hijri.year].join(' ');
 }
 
 
 displayTime();
+
+setInterval(() => {
+  const hours = document.querySelector('.timeRemaining > .hours');
+  const minutes = document.querySelector('.timeRemaining > .minutes');
+  const seconds = document.querySelector('.timeRemaining > .seconds');
+
+  const d = new Date()
+
+  const [iftarHours, iftarMinutes] = iftarTime.split(':');
+  const iftarSeconds = '00';
+
+  const remaining = timeSubtract({
+    hours: iftarHours,
+    minutes: iftarMinutes,
+    seconds: iftarSeconds
+  }, {
+    hours: d.getHours(),
+    minutes: d.getMinutes(),
+    seconds: d.getSeconds()
+  });
+
+  // if(remaining.hours === 0 && remaining.minutes === 0 && remaining.seconds === 0)
+    // document.querySelector('.timeRemaining').classList.add('timesup');
+
+  hours.textContent = intToStringof2(remaining.hours)
+  minutes.textContent = intToStringof2(Math.abs(remaining.minutes))
+  seconds.textContent = intToStringof2(Math.abs(remaining.seconds))
+}, 1000);
+
+function timeSubtract(time1, time2)
+{
+  const time = { ...time1 };
+
+  if(time.hours < time2.hours)
+  {
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  } else if(time.hours === time2.hours) {
+    if(time.minutes < time2.minutes) {
+      return {
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      };
+    } else if(time.minutes === time2.minutes && time.seconds <= time2.seconds) {
+        return {
+          hours: 0,
+          minutes: 0,
+          seconds: 0
+        };
+    }
+  }
+
+  if(time1.seconds < time2.seconds)
+  {
+    --time.minutes;
+    time.seconds = (60 + parseInt(time.seconds)) - time2.seconds;
+  } else {
+    time.seconds = time.seconds - time2.seconds;
+  }
+  
+  if(time1.minutes < time2.minutes) {
+    --time.hours;
+    time.minutes = (60 + parseInt(time.minutes)) - time2.minutes;
+  } else {
+    time.minutes = time.minutes - time2.minutes;
+  }
+
+
+  time.hours = time.hours - time2.hours;
+
+  return time;
+}
